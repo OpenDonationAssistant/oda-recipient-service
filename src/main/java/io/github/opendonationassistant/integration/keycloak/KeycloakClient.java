66449a25d2b683d6ff00a92d3d6beabe31017e4c
stCarolas <stcarolas@gmail.com>
2025-10-01 08:19:56 +0300

    add consumes header and JsonProperty to keycloak client

--- src/main/java/io/github/opendonationassistant/integration/keycloak/KeycloakClient.java
+++ src/main/java/io/github/opendonationassistant/integration/keycloak/KeycloakClient.java
@@ -7,17 +7,19 @@ import io.micronaut.http.client.annotation.Client;
 import io.micronaut.serde.annotation.Serdeable;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Client("keycloak")
 public interface KeycloakClient {
   @Post(
     value = "/realms/ODA/protocol/openid-connect/token",
-    produces = MediaType.APPLICATION_FORM_URLENCODED
+    produces = MediaType.APPLICATION_FORM_URLENCODED,
+    consumes = MediaType.APPLICATION_JSON
   )
   CompletableFuture<GetAccessRecordResponse> getAccessToken(
     @Body Map<String, String> params
   );
 
   @Serdeable
-  public static record GetAccessRecordResponse(String accessToken) {}
+  public static record GetAccessRecordResponse(@JsonProperty("access_token") String accessToken) {}
 }
