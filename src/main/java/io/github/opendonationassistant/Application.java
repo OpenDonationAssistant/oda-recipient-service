462905fd9ebbb91dd9aea28f022488a38b3d11ff
stCarolas <stcarolas@gmail.com>
2026-07-23 09:10:54 +0300

    add remote cache configuration

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -9,6 +9,7 @@ import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
 import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.rabbitmq.connect.ChannelPool;
@@ -20,6 +21,8 @@ import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import java.util.List;
 import java.util.Map;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 
 @OpenAPIDefinition(info = @Info(title = "oda-recipient-service"))
 @Factory
@@ -52,6 +55,31 @@ public class Application {
         Exchange.Exchange("rpc", Map.of("token", TokenRequestHandler.QUEUE))
       )
     );
+  }
+
+  @Singleton
+  public RemoteCacheManager remoteCacheManager(
+    @Value("${infinispan.client.hotrod.server.host}") String host,
+    @Value("${infinispan.client.hotrod.server.port}") int port,
+    @Value(
+      "${infinispan.client.hotrod.security.authentication.username}"
+    ) String username,
+    @Value(
+      "${infinispan.client.hotrod.security.authentication.password}"
+    ) String password
+  ) {
+    var conf = new ConfigurationBuilder()
+      .addServer()
+      .host(host)
+      .port(port)
+      .security()
+      .authentication()
+      .username(username)
+      .password(password)
+      .build();
+    var manager = new RemoteCacheManager(conf);
+    manager.start();
+    return manager;
   }
 
   @Singleton
