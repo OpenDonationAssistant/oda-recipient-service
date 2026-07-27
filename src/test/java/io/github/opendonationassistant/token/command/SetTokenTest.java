4433727513ebac63c426527c2c6a8e9f913eea07
stCarolas <stcarolas@gmail.com>
2026-07-28 00:56:46 +0300

    cleanup tests

--- src/test/java/io/github/opendonationassistant/token/command/SetTokenTest.java
+++ src/test/java/io/github/opendonationassistant/token/command/SetTokenTest.java
@@ -15,7 +15,6 @@ import org.instancio.junit.Given;
 import org.instancio.junit.InstancioExtension;
 import org.instancio.junit.WithSettings;
 import org.instancio.settings.Settings;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
